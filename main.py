5e049bd4c18296c4b7238d841cccc389b964d0f5
Ahantya <ahantyasharma57@gmail.com>
2025-06-22 17:23:12 -0500

    cleaned up PCF dataset

--- main.py
+++ main.py
@@ -4,4 +4,12 @@ import pandas as pd
 PCF = pd.read_csv("PCF.csv")
 CEDA = pd.read_csv("CEDA.csv")
 
-# my name is 
+
+# first 25 rows are just text, so we need to remove them
+PCF = PCF.iloc[26: ]
+# first column is just empty
+PCF = PCF.drop(PCF.columns[0], axis=1)
+# product code and description aren't really needed
+PCF = PCF.drop(index=[27, 28])
+
+print(PCF.head())
